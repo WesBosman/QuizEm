d4574f6ff77dd13ec68a433b4140431a21c1488a
richardsonbj <richardsonbj@vcu.edu>
2015-10-01 02:49:19 -0400

    Updated button clicker test; Made half working quiz maker test.
    Button clicker test now goes through all activities.
    
    Made a quiz maker test which almost works. It is able to edit text but
    it will not edit the answer test and fails when it tries (for no good
    reason). It does edit the title and the question fields.
    
    Started a delete test.

--- MyApplication/app/src/androidTest/java/edu/vcu/wes/myapplication/EspressoTest.java
+++ MyApplication/app/src/androidTest/java/edu/vcu/wes/myapplication/EspressoTest.java
@@ -3,10 +3,16 @@ package edu.vcu.wes.myapplication;
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import android.test.suitebuilder.annotation.LargeTest;
+import android.widget.EditText;
 
 //import junit.framework.TestResult;
 //import org.junit.Rule;
@@ -34,11 +40,45 @@ import android.test.suitebuilder.annotation.LargeTest;
     }
 
     /**
-     * This method checks that the buttons on the Main Screen can be clicked.
+     * This method checks that the buttons currently in the app can be clicked.
      */
     public void testCheckButtons() {
         onView(withId(R.id.quiz_button)).perform(click());
-        //getActivity();
-        //onView(withId(R.id.flash_button)).perform(click());
+        //assertEquals("MainActivity" , getActivity());
+        pressBack();
+        onView(withId(R.id.flash_button)).perform(click());
+        pressBack();
+        onView(withId(R.id.quiz_button)).perform(click());
+        onView(withId(R.id.quiz_make)).perform(click());
+        pressBack();
+        onView(withId(R.id.quiz_take)).perform(click());
+        //pressBack();
+        onView(withId(R.id.quiz_all)).perform(click());
+        pressBack();
+        pressBack();
+
     }
+
+    /**This one tests our make a quiz function. Currently half works.*/
+    public void testMakeQuiz() {
+        onView(withId(R.id.quiz_button)).perform(click());
+        onView(withId(R.id.quiz_make)).perform(click());
+        onView(withId(R.id.editTextTitle)).perform((click()), replaceText("Please Work")); /**Had to use replace text here rather than edit text because of a glitch in espresso with how the keyboard works */
+        //.perform(typeText("Please Work"));
+        pressBack();
+        onView(withId(R.id.editTextQuestion)).perform((click()), replaceText("I got it to work."));
+        pressBack();
+        onView(withId(R.id.editTextAnswer)).perform((click()), replaceText("feels good man"));/**It stops working here for no reason*/
+        pressBack();
+        onView(withId(R.id.submit_button)).perform(click());
+        pressBack();
+        pressBack();
+    }
+
+    /** And this one tests our current delete function */
+    /**public void testDeleteQuiz() {
+        onView(withId(R.id.quiz_button)).perform(click());
+        onView(withId(R.id.quiz_all)).perform(click());
+        onView(withId(R.id.simple_expandable_list_item_1)).perform(click());
+    }*/
 }
