9f199700964aea54e6b6bea18e4f230b0c3526e4
Tom <oakleytb@vcu.edu>
2015-11-21 19:58:56 -0500

    Changing launcher icon, home button fixed wes did this

--- MyApplication/app/src/main/java/edu/vcu/wes/myapplication/CustomArrayAdapter.java
+++ MyApplication/app/src/main/java/edu/vcu/wes/myapplication/CustomArrayAdapter.java
@@ -3,6 +3,7 @@ package edu.vcu.wes.myapplication;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
